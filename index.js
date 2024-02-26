91096949d33a9aa7d3ab53faaf576d81d0789205
Srikanth K <kolleparasrikanth@gmail.com>
2024-02-26 19:28:06 +0530

    session part 1

---
+++ index.js
@@ -0,0 +1,70 @@
+import express from 'express';
+import ProductsController from './src/controllers/product.controller.js';
+import UserController from './src/controllers/user.controller.js';
+import ejsLayouts from 'express-ejs-layouts';
+import path from 'path';
+import validationMiddleware from './src/middlewares/validation.middleware.js';
+import { uploadFile } from './src/middlewares/file-upload.middleware.js';
+import session from 'express-session';
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(session({
+  secret: 'SecretKey',
+  resave: false,
+  saveUninitialized: true,
+  cookie:{secure: false},
+}))
+
+const productsController =
+  new ProductsController();
+const usersController = new UserController();
+
+app.use(ejsLayouts);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set(
+  'views',
+  path.join(path.resolve(), 'src', 'views')
+);
+
+app.get('/register', usersController.getRegister);
+app.get('/login', usersController.getLogin);
+app.post('/login', usersController.postLogin);
+app.post(
+  '/register',
+  usersController.postRegister
+);
+app.get('/', productsController.getProducts);
+app.get(
+  '/add-product',
+  productsController.getAddProduct
+);
+
+app.get(
+  '/update-product/:id',
+  productsController.getUpdateProductView
+);
+
+app.post(
+  '/delete-product/:id',
+  productsController.deleteProduct
+);
+
+app.post(
+  '/',
+  uploadFile.single('imageUrl'),
+  validationMiddleware,
+  productsController.postAddProduct
+);
+
+app.post(
+  '/update-product',
+  productsController.postUpdateProduct
+);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
